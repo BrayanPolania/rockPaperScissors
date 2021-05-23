a22938005d67871460e3950417834420916dff99
Brayan Polania <brayanstevenpolaniaacosta@gmail.com>
2021-05-22 20:44:06 -0500

    add winner

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+
 //RULES
 const btnRules = document.getElementById("btnRules");
 const rules = document.getElementById("rules");
@@ -18,79 +19,111 @@ function closeRules () {
 const btnPaper = document.getElementById("btnPaper");
 const btnScissors = document.getElementById("btnScissors");
 const btnRock = document.getElementById("btnRock");
-  //sections
+//sections
 const userPick = document.getElementById("userPick");
 const displayPick = document.getElementById("displayPick");
+const result = document.getElementById("result");
     //YOU PICKED
 const youPickedBorder = document.getElementById("youPickedBorder");
 const youPickedImg = document.getElementById("youPickedImg");
+    //house pick
+const housePickedBorder = document.getElementById("housePickedBorder");
+const housePickedImg = document.getElementById("housePickedImg");
+const housePickedBtn = document.getElementById("housePickedBtn");
+const win = document.getElementById("win");
+
+let optionUser;
+let optionHome;
 
 btnPaper.addEventListener("click", pickPaper);
 btnScissors.addEventListener("click", pickScissors);
 btnRock.addEventListener("click", pickRock);
 
-function pickPaper () {
-  console.log("Paper");
+function pick (option) {
   userPick.style.display = "none";
   displayPick.style.display = "flex";
-  youPickedBorder.classList.add ("paper");
-  youPickedImg.src = "images/icon-paper.svg";
+  youPickedBorder.classList.add (`${option}`);
+  youPickedImg.src = `images/icon-${option}.svg`;
+  optionUser = option;
+  console.log(optionUser);
   housePick();
+}
+function pickPaper () {
+  pick("paper");
 }
 function pickScissors () {
-  console.log("Scissors");
-  userPick.style.display = "none";
-  displayPick.style.display = "flex";
-  youPickedBorder.classList.add ("scissors");
-  youPickedImg.src = "images/icon-scissors.svg";
-  housePick();
+  pick("scissors");
 }
 function pickRock () {
-  console.log("Rock");
-  userPick.style.display = "none";
-  displayPick.style.display = "flex";
-  youPickedBorder.classList.add ("rock");
-  youPickedImg.src = "images/icon-rock.svg";
-  housePick();
+  pick("rock");
 }
 
 //THE HOUSE PICK
-const housePickedBorder = document.getElementById("housePickedBorder");
-const housePickedImg = document.getElementById("housePickedImg");
-const housePickedBtn = document.getElementById("housePickedBtn");
-
+function pickHome (option) {
+  optionHome = option;
+  console.log(optionHome);
+  setTimeout(function () {
+    housePickedBtn.style.backgroundColor = "#fff";
+    housePickedBtn.style.boxShadow = "inset 0 0.4375rem 0.0625rem #2a46c047";
+    housePickedBorder.classList.add (`${option}`);
+    housePickedImg.src = `images/icon-${option}.svg`;
+  }, 1000);
+  winner();
+}
 
 function housePick () {
   housePickedBtn.style.backgroundColor = "#16213d";
   housePickedBtn.style.boxShadow = "none";
   let x = numAleatorio(1,3);
-  console.log(x);
   if (x === 1) {
-    setTimeout(function(){
-        housePickedBtn.style.backgroundColor = "#fff";
-        housePickedBtn.style.boxShadow = "inset 0 0.4375rem 0.0625rem #2a46c047";
-      housePickedBorder.classList.add ("paper");
-      housePickedImg.src = "images/icon-paper.svg";
-      
-    }, 1000);
+    pickHome ("paper");
   }
   if (x === 2) {
-    setTimeout(function(){
-        housePickedBtn.style.backgroundColor = "#fff";
-        housePickedBtn.style.boxShadow = "inset 0 0.4375rem 0.0625rem #2a46c047";
-      housePickedBorder.classList.add ("scissors");
-      housePickedImg.src = "images/icon-scissors.svg";
-    }, 1000);
+    pickHome ("scissors");
   }
   if (x === 3) {
-    setTimeout(function(){
-        housePickedBtn.style.backgroundColor = "#fff";
-        housePickedBtn.style.boxShadow = "inset 0 0.4375rem 0.0625rem #2a46c047";
-      housePickedBorder.classList.add ("rock");
-      housePickedImg.src = "images/icon-rock.svg";
-    }, 1000);
+    pickHome ("rock");
   }
 }
+
+//PLAY AGAIN
+// setTimeout(function(){
+//   result.style.display = "flex";
+// }, 1000);
+function winner () {
+  if (optionUser === optionHome) {
+    console.log("it's a tie");
+    win.innerText = "It's a tie";
+    setTimeout(function(){
+      result.style.display = "flex";
+    }, 2000);
+  }
+  else if (
+  (optionUser === "rock" & optionHome === "scissors") || 
+  (optionUser === "paper" & optionHome === "rock") || 
+  (optionUser === "scissors" & optionHome === "paper")
+  ) {
+    console.log("you win");
+    win.innerText = "you win";
+    setTimeout(function(){
+      result.style.display = "flex";
+    }, 2000);
+  }
+  else {
+    console.log("you lost");
+    win.innerText = "you lost";
+    setTimeout(function(){
+      result.style.display = "flex";
+    }, 2000);
+  }
+}
+
+
+
+
+
+
+
   
   //FUNCTIONS
   function numAleatorio(min, max) {
